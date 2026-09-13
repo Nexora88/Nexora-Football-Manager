620257cd3dcbd3570883f6a23a790759bb99f57e
Nexora88 <ahmeteymenbakrac@gmail.com>
2026-09-13 10:51:09 +0300

    feat: add player database foundation

---
+++ game/players.js
@@ -0,0 +1,26 @@
+const players = [
+  {
+    name: "Emir Kaya",
+    age: 18,
+    position: "ST",
+    rating: 59,
+    potential: 88,
+    personality: "Çalışkan"
+  },
+  {
+    name: "Can Demir",
+    age: 22,
+    position: "MC",
+    rating: 67,
+    potential: 75,
+    personality: "Profesyonel"
+  },
+  {
+    name: "Burak Aydın",
+    age: 25,
+    position: "GK",
+    rating: 70,
+    potential: 72,
+    personality: "Lider"
+  }
+];
